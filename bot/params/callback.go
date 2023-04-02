ea3b7a004dcf9590a12b438692b6bc4ad2d28d7b
billikeu <billikeu@gmail.com>
2023-04-02 13:30:35 +0800

    add chunk index to record CallParams

--- bot/params/callback.go
+++ bot/params/callback.go
@@ -1,26 +1,28 @@
 package params
 
 type CallParams struct {
-	MsgId    string
-	ParentId string
-	Chunk    string
-	Text     string
-	Done     bool
-	Model    string
+	MsgId      string
+	ParentId   string
+	Chunk      string
+	Text       string
+	Done       bool
+	Model      string
+	ChunkIndex int
 }
 
 // create params for ask callback
 /*
 callback(NewCallParams(msgId, parentId, chunk, text, false), err)
 */
-func NewCallParams(msgId, parentId, chunk, text, model string, done bool) *CallParams {
+func NewCallParams(msgId, parentId, chunk, text, model string, done bool, chunkIndex int) *CallParams {
 	p := &CallParams{
-		MsgId:    msgId,
-		ParentId: parentId,
-		Chunk:    chunk,
-		Text:     text,
-		Done:     done,
-		Model:    model,
+		MsgId:      msgId,
+		ParentId:   parentId,
+		Chunk:      chunk,
+		Text:       text,
+		Done:       done,
+		Model:      model,
+		ChunkIndex: chunkIndex,
 	}
 	return p
 }
